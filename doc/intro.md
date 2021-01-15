c0fc23552ff2332bb31eb9bc8a1037ed45c88b2f
Gabriel <gabrieldavid_98@hotmail.com>
2021-01-14 23:50:53 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to europe-by-train
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/europe_by_train/core_test.clj
@@ -0,0 +1,7 @@
+(ns europe-by-train.core-test
+  (:require [clojure.test :refer :all]
+            [europe-by-train.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
